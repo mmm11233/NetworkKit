95b89551e2f871dfe0eadf8f7ae798c130b76336
Mariam Joglidze <mariamjoglidze1@gmail.com>
2026-01-31 02:12:54 +0400

    make more flexible

--- Sources/NetworkKit/NetworkClient.swift
+++ Sources/NetworkKit/NetworkClient.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  NetworkClient.swift
 //  NetworkKit
 //
 //  Created by Mariam Joglidze on 29.01.26.
@@ -7,9 +7,53 @@
 
 import Foundation
 
-public protocol NetworkClient {
-    func request<T: Decodable>(
+public final class NetworkClient: NetworkClient {
+
+    private let baseURL: String
+    private let session: URLSession
+
+    public init(
+        baseURL: String,
+        session: URLSession = .shared
+    ) {
+        self.baseURL = baseURL
+        self.session = session
+    }
+
+    public func request<T: Decodable>(
         endpoint: Endpoint,
         responseType: T.Type
-    ) async throws -> T
+    ) async throws -> T {
+
+        guard var components = URLComponents(string: baseURL) else {
+            throw NetworkError.invalidURL
+        }
+
+        components.path += endpoint.path
+        components.queryItems = endpoint.queryItems
+
+        guard let url = components.url else {
+            throw NetworkError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        request.allHTTPHeaderFields = endpoint.headers
+
+        let (data, response) = try await session.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw NetworkError.serverError(statusCode: httpResponse.statusCode)
+        }
+
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw NetworkError.decodingError
+        }
+    }
 }
---
+++ Sources/NetworkKit/NetworkClientProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  NetworkKit
+//
+//  Created by Mariam Joglidze on 29.01.26.
+//
+
+import Foundation
+
+public protocol NetworkClientProtocol {
+    func request<T: Decodable>(
+        endpoint: Endpoint,
+        responseType: T.Type
+    ) async throws -> T
+}
